bee83c7f1b0e78a640a667708009a7b894362764
Luc Schurgers <Luc@DNABLOCK-M1-AIR.local>
2025-08-21 16:54:03 -0700

    Fix root page redirect to home instead of search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 
 export default function RootPage() {
-  // Redirect to search page which doesn't require initial database access
-  redirect("/search")
+  // Redirect to the home page
+  redirect("/home")
 }
